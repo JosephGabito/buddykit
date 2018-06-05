7747ff7b81b9ccf1eb8b5a203b7fa07774707fa0
Joseph Gabito <codehaiku.io@gmail.com>
2018-06-05 19:44:16 +0800

    added iamges delete func

--- buddykit.php
+++ buddykit.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://buddykit.io/
  * Author: Dunhakdis, Joseph G.
  * Author URI: https://dunhakdis.com/
- * Version: 0.0.1
+ * Version: 0.0.2
  * Text Domain: buddykit
  * License: GPLv3
  * Domain Path: /src/languages
