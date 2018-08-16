fa905a7a3ff9be8a18014a88abbe7d405829b2f5
Joseph Gabito <codehaiku.io@gmail.com>
2018-08-16 23:16:19 +0800

    new field: checkbox

---
+++ src/includes/settings/field-types/checkbox.php
@@ -0,0 +1,57 @@
+<?php
+namespace OptionKit\FieldTypes;
+
+class CheckBox {
+	
+	protected $atts = array();
+
+	public function __construct( $args ) {
+		$this->atts = $args;
+	}
+
+	public function display() {
+		
+		do_action('optionkit-before-field');
+
+		if ( empty( $this->atts['options'] ) ) {
+			esc_html_e('options argument for field type "radio" is not defined', 'optionkit');
+			return;
+		}
+
+		
+		
+		$counter = 0;
+
+		foreach( $this->atts['options'] as $key => $val ): ?>
+
+			<?php $checked = ''; ?>
+			
+			<?php $name = sprintf("%s[]", $this->atts['id']); ?>
+
+			<?php $saved_option = get_option( $this->atts['id'] ); ?>
+
+			<?php if ( in_array( $key, $saved_option ) ) { ?>
+				<?php $checked = 'checked'; ?>
+			<?php } ?>
+
+			<?php $field_id = sprintf("%s-%s",$this->atts['id'], $key ); ?>
+			
+			<p class="optionkit-checkbox">
+				<label for="<?php echo esc_attr( $field_id ); ?>">
+				<input <?php echo esc_attr( $checked ); ?> value="<?php echo esc_attr( $key); ?>" type="checkbox" id="<?php echo esc_attr( $field_id ); ?>" name="<?php echo esc_attr( $name ); ?>" />
+				<?php echo esc_html( $val ); ?>
+				</label>
+			</p>
+			
+			<?php $counter ++ ;?>
+
+		<?php endforeach; ?>
+
+		<p class="description">
+			<?php echo wp_kses_post( $this->atts['description'] ); ?>
+		</p>
+		<?php
+		do_action('optionkit-after-field');
+		return;
+	}
+}
