13180d1c29860120c9b97cfb27ed1d3c245843cc
Joseph Gabito <codehaiku.io@gmail.com>
2018-08-17 16:08:42 +0800

    added new fields;

---
+++ src/includes/settings/assets/js/optionkit.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($){
+	'use strict';
+	// Add Color Picker to all inputs that have 'data-colorpicker=optionkit-wp-colorpicker' class
+    $(function() {
+        $('input[data-colorpicker=optionkit-wp-colorpicker]').wpColorPicker();
+    });
+
+    // Media Upload.
+    // Set all variables to be used in scope
+  	var frame,
+    metaBox = $('#meta-box-id.postbox'), // Your meta box id here
+    addImgLink = metaBox.find('.upload-custom-img'),
+    delImgLink = metaBox.find( '.delete-custom-img'),
+    imgContainer = metaBox.find( '.custom-img-container'),
+    imgIdInput = metaBox.find( '.custom-img-id' );
+
+    $('input[data-media-upload-button=yes]').on('click', function(e){
+    	e.preventDefault();
+    	var input = $(this);
+    	if ( frame ) {
+    		frame.open();
+    		return;
+    	}
+
+    	frame = wp.media({
+	    	title: 'Select or Upload Media',
+	      	button: {
+	        	text: 'Use this media'
+	     	},
+	     	library: {
+	     		type: ['image']
+	     	},
+	     	multiple: false  // Set to true to allow multiple files to be selected
+	    });
+
+	    // When an image is selected in the media frame...
+	    frame.on( 'select', function() {
+	      	var attachment = frame.state().get('selection').first().toJSON();
+	     		$(input.attr('data-input-id')).val( attachment.id );
+	     		$(input.attr('data-preview-id')).html("<img class='optionkit-preview' src='"+attachment.url+"' />");
+	    });
+
+	    // Finally, open the modal on click
+	    frame.open();
+	  });
+
+    $('.optionkit-image-upload-delete').on('click', function(e){
+    	e.preventDefault();
+    	var input = $(this);
+    	$(input.attr('data-input-id')).val('');
+	    $(input.attr('data-preview-id')).html("");
+	    $('input[data-media-upload-button=yes]').removeClass('hidden');
+	    $(this).addClass('hidden');
+    })
+    
+});
+
+
+    
+
